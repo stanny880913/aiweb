a9a0e6062454a95ffc80b8ca0e1632d1179455fd
StannyHo <stanny11122@gmail.com>
2023-01-27 12:07:29 +0800

    add backend upload image func

--- docker-vue/src/main.js
+++ docker-vue/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App);
+// app.provide('axios', axios);
+app.mount('#app'); //mounte  掛載
--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,15 +1,20 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Worlddddddd"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    return {"filenames": [file.filename for file in files]}
